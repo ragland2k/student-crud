086f8cfa18e56fb57a069809ddeff1b360e38a71
Sanjithwebersink <sanjithwebersink@gmail.com>
2025-12-16 16:38:49 +0530

    Added Students link to navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('students.index')" :active="request()->routeIs('students.*')">
+    Students
+</x-nav-link>
+
                 </div>
             </div>
 
